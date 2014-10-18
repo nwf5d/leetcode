9f505b57aeb84e9e697a0c7ede0da5265cd52412
nwf5d <nwfengwolf@gmail.com>
2014-10-18 10:35:02 +0800

    first commit

---
+++ python/hello_world.py
@@ -0,0 +1,14 @@
+#!/bin/env python
+#encoding:gbk
+
+import os
+import sys
+
+def test():
+	print "abc"
+	print "ddd"
+
+if __name__ == '__main__':
+	test()
+	print "hello world!"
+	print "hellow rwrold!"
